dbaf4621173cf27bec6eabf904c6302ed86e6ebd
Bruno Mendes <bd_mendes@outlook.com>
2025-06-30 00:55:27 +0100

    Add MockedMethod class

---
+++ src/main/scala/com/bdmendes/smockito/MockedMethod.scala
@@ -0,0 +1,31 @@
+package com.bdmendes.smockito
+
+/** Representation of a mocked method, implicitly converted from an eta-expanded
+  * method.
+  *
+  * @tparam P
+  *   the (possibly tupled) types of the method parameters.
+  * @tparam R
+  *   the return type.
+  */
+abstract class MockedMethod[P, R]:
+
+  /** Sets up an implementation for the method for the set of arguments in the
+    * domain of the given partial function. If this method is called with other
+    * arguments, it will throw a [[IllegalArgumentException]].
+    *
+    * @param f
+    *   the method implementation.
+    */
+  def returns(f: PartialFunction[P, R]): Unit
+
+  /** The list of arguments this method received on each call, in chronological
+    * order. Returns multiple arguments as a tuple.
+    *
+    * You may query the size of the result list to quickly match against the
+    * number of times this method was called.
+    *
+    * @return
+    *   the received arguments per call.
+    */
+  def calls: List[R]
