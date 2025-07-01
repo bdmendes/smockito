221c7e5345bf3531bcd193bb09b6d9dc3b0f696c
Bruno Mendes <bd_mendes@outlook.com>
2025-07-01 23:19:49 +0100

    Add on method with 1 parameter

--- src/main/scala/com/bdmendes/smockito/Mock.scala
+++ src/main/scala/com/bdmendes/smockito/Mock.scala
@@ -1,9 +1,29 @@
 package com.bdmendes.smockito
 
+import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
 import scala.reflect.ClassTag
 
 opaque type Mock[T] = T
+
+extension [T](mock: Mock[T])
+
+  def on[A1, A2, R1, R2](
+      method: T => A1 => R1
+  )(using A1 =:= A2, R1 =:= R2)(stub: PartialFunction[A2, R2]): Mock[T] =
+    Mockito
+      .when(method(mock).apply(ArgumentMatchers.any()))
+      .thenAnswer { invocation =>
+        val argument = invocation.getArgument[A1](0)
+        stub.applyOrElse(
+          argument,
+          _ =>
+            throw new IllegalArgumentException(
+              s"Mocked method received unexpected parameters: $argument"
+            )
+        )
+      }
+    mock
 
 object Mock:
 
--- src/test/scala/com/bdmendes/smockito/SmockitoSpec.scala
+++ src/test/scala/com/bdmendes/smockito/SmockitoSpec.scala
@@ -2,16 +2,15 @@ package com.bdmendes.smockito
 
 import com.bdmendes.smockito.SmockitoSpec.*
 import org.mockito.Mockito
+import scala.compiletime.testing.typeChecks
 
 class SmockitoSpec extends munit.FunSuite with Smockito:
 
   test("wrap a raw Mockito instance"):
     val repository = mock[Repository[User]]
 
-    // A Mock[T] is implicitly converted to a `T`, so one can use Mockito
-    // methods on it.
-    // Of course, that is not the purpose of this library, but this is a
-    // showcase.
+    // A Mock[T] is implicitly converted to a `T`, so one can use Mockito methods on it.
+    // Of course, that is not the purpose of this library, but this is a showcase.
     Mockito.when(repository.get).thenReturn(mockUsers)
     Mockito.when(repository.exists("bdmendes")).thenReturn(true)
 
@@ -25,12 +24,38 @@ class SmockitoSpec extends munit.FunSuite with Smockito:
 
     Mockito.verify(repository).exists("bdmendes")
 
+  test("provide a method to setup partial method stubs, on methods with 1 parameter"):
+    val repository =
+      mock[Repository[User]].on(_.exists) {
+        case name if name.endsWith("mendes") =>
+          true
+      }
+
+    assert(typeChecks("repository.on(_.exists) { case \"bdmendes\" => true }"))
+    assert(typeChecks("repository.on(_.exists) { _ => true }"))
+    assert(!typeChecks("repository.on(_.exists) { case \"bdmendes\" => 1 }"))
+    assert(!typeChecks("repository.on(_.exists) { case 1 => \"bdmendes\" }"))
+
+    assertEquals(repository.exists("bdmendes"), true)
+    intercept[IllegalArgumentException](repository.exists("spider"))
+
+//  test("provide a method to setup partial method stubs, on methods with 2 parameters"):
+//    val a = mock[Repository[User]].getWith
+//    val repository =
+//      mock[Repository[User]].on(_.getWith) { case (start: String, end: String) =>
+//        mockUsers.filter(u => u.username.startsWith(start) && u.username.endsWith(end))
+//      }
+//
+//    assertEquals(repository.getWith("bd", ""), List(User("bdmendes")))
+//    assertEquals(repository.getWith("", "mendes"), List(User("bdmendes"), User("apmendes")))
+
 object SmockitoSpec:
 
   abstract class Repository[T](val name: String):
     val longName = s"${name}Repository"
     def get: List[T]
     def exists(username: String): Boolean
+    def getWith(startsWith: String, endsWith: String): List[T]
 
   class Service[T](repository: Repository[T]):
     def getWith[K](f: T => Boolean): List[T] = repository.get.filter(f)
@@ -39,9 +64,4 @@ object SmockitoSpec:
   case class User(username: String)
 
   private val mockUsers =
-    List(
-      User("bdmendes"),
-      User("apmendes"),
-      User("sirze01"),
-      User("fernandorego")
-    )
+    List(User("bdmendes"), User("apmendes"), User("sirze01"), User("fernandorego"))
