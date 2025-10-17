7f485ad1d3c7e029cef0a118f866dab3cba0e77a
Bruno Mendes <bd_mendes@outlook.com>
2025-10-17 11:26:39 +0100

    Bump scala to 3.3.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,12 +4,13 @@ object Dependencies {
 
   object Versions {
     // We use the latest Scala LTS version, as advised for libraries.
-    lazy val scala = "3.3.6"
+    lazy val scala = "3.3.7"
     // We want to be conservative with the Java version, but not too much.
     lazy val java = "17"
     lazy val mockito = "5.20.0"
+    lazy val munit = "1.2.1"
   }
 
   lazy val mockito = "org.mockito" % "mockito-core" % Versions.mockito
-  lazy val munit = "org.scalameta" %% "munit" % "1.2.1"
+  lazy val munit = "org.scalameta" %% "munit" % Versions.munit
 }
