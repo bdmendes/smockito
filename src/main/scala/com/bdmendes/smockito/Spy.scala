22859ef21267af750686740173bd894505fa14c9
Bruno Mendes <bd_mendes@outlook.com>
2026-03-13 23:32:00 +0000

    Revert "Use delegation only when spy fails"
    This reverts commit 30f8144c543395527c5c21189918fec432eba7fa.

--- src/main/scala/com/bdmendes/smockito/Spy.scala
+++ src/main/scala/com/bdmendes/smockito/Spy.scala
@@ -2,7 +2,6 @@ package com.bdmendes.smockito
 
 import org.mockito.AdditionalAnswers
 import org.mockito.Mockito
-import org.mockito.exceptions.base.MockitoException
 import scala.reflect.ClassTag
 
 /** A `Spy` is a mock whose default answer is to forward all method calls to a real instance, unless
@@ -13,15 +12,9 @@ opaque type Spy[+T] <: Mock[T] = Mock[T]
 private object Spy:
 
   def apply[T](realInstance: T)(using ct: ClassTag[T]): Spy[T] =
-    try
-      Mockito.spy(realInstance).asInstanceOf[Mock[T]]
-    catch
-      case _: MockitoException =>
-        // A "delegating mock" is less powerful than a spy, since it cannot record internal
-        // interactions, but it can be used as a fallback when the type cannot be spied.
-        Mockito
-          .mock(
-            ct.runtimeClass.asInstanceOf[Class[T]],
-            Mockito.withSettings().defaultAnswer(AdditionalAnswers.delegatesTo(realInstance))
-          )
-          .asInstanceOf[Mock[T]]
+    Mockito
+      .mock(
+        ct.runtimeClass.asInstanceOf[Class[T]],
+        Mockito.withSettings().defaultAnswer(AdditionalAnswers.delegatesTo(realInstance))
+      )
+      .asInstanceOf[Mock[T]]
