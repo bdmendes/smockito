7e9fa03923d6da3ae0d5bb3c0708fe5e42e2093a
Bruno Mendes <bd_mendes@outlook.com>
2026-03-13 23:32:05 +0000

    Revert "Use native delegation answer"
    This reverts commit ff8ca75df1a0e2acb3c55ebc1bfe86363f4a4f64.

--- src/main/scala/com/bdmendes/smockito/Spy.scala
+++ src/main/scala/com/bdmendes/smockito/Spy.scala
@@ -1,7 +1,8 @@
 package com.bdmendes.smockito
 
-import org.mockito.AdditionalAnswers
 import org.mockito.Mockito
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
 import scala.reflect.ClassTag
 
 /** A `Spy` is a mock whose default answer is to forward all method calls to a real instance, unless
@@ -11,10 +12,15 @@ opaque type Spy[+T] <: Mock[T] = Mock[T]
 
 private object Spy:
 
+  class SpyAnswer(obj: Any) extends Answer[Any]:
+
+    override def answer(invocation: InvocationOnMock): Any =
+      invocation.getMethod.invoke(obj, invocation.getRawArguments*)
+
   def apply[T](realInstance: T)(using ct: ClassTag[T]): Spy[T] =
     Mockito
       .mock(
         ct.runtimeClass.asInstanceOf[Class[T]],
-        Mockito.withSettings().defaultAnswer(AdditionalAnswers.delegatesTo(realInstance))
+        Mockito.withSettings().defaultAnswer(SpyAnswer(realInstance))
       )
       .asInstanceOf[Mock[T]]
