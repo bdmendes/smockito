304bed5ea3e0bf436199cdde6df04e6a30ca613f
Bruno Mendes <bd_mendes@outlook.com>
2026-04-28 17:26:01 +0100

    Test val spawn

--- src/test/scala/com/bdmendes/smockito/MockedMethodSpec.scala
+++ src/test/scala/com/bdmendes/smockito/MockedMethodSpec.scala
@@ -6,6 +6,7 @@ class MockedMethodSpec extends munit.FunSuite:
     // scalafmt: { maxColumn = 150 }
     type D = Int
     val _: MockedMethod[EmptyTuple, D] = () => 1
+    val _: MockedMethod[EmptyTuple, D] = 1
     val _: MockedMethod[Tuple1[D], D] = (_: D) => 1
     val _: MockedMethod[(D, D), D] = (_: D, _: D) => 1
     val _: MockedMethod[(D, D, D), D] = (_: D, _: D, _: D) => 1
