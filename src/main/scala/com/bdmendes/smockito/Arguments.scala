0c4e75f59e7d403716f9204da4cd717d12d243f9
Bruno Mendes <bd_mendes@outlook.com>
2026-09-20 14:38:59 +0100

    Make NamedArguments covariant

--- src/main/scala/com/bdmendes/smockito/Arguments.scala
+++ src/main/scala/com/bdmendes/smockito/Arguments.scala
@@ -17,7 +17,7 @@ type Arguments[N <: Tuple, A <: Tuple] =
 
 object Arguments:
 
-  opaque type NamedArguments[N <: Tuple, A <: Tuple] >: A <: NamedTuple.NamedTuple[N, A] =
+  opaque type NamedArguments[N <: Tuple, +A <: Tuple] >: A <: NamedTuple.NamedTuple[N, A] =
     NamedTuple.NamedTuple[N, A]
 
   // Named arguments retain positional access.
